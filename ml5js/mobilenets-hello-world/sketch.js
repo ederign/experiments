abe1373a94a799d418860b4d79c5752e37bd1ffb
ederign <ignatowicz@gmail.com>
2018-08-15 21:30:35 -0400

    Dorinha on screen

--- ml5js/mobilenets-hello-world/sketch.js
+++ ml5js/mobilenets-hello-world/sketch.js
@@ -1,45 +1,33 @@
 let mobilenet;
-let dora;
-let eder;
-
-
+let video;
+let label = '';
 
 function modelReady() {
-    console.log("model is ready!");
-    mobilenet.predict(dora, gotResults);
+    mobilenet.predict(gotResults);
 }
 
-function gotResults(error, results){
-    if(error){
+function gotResults(error, results) {
+    if (error) {
         console.error(error);
     }
-    else{
-        console.log(results);
-        let label = results[0].className;
-        let prob = results[0].probability;
-        fill(0);
-        textSize(64);
-        text(label, 10, height - 50);
-        createP(label);
-        createP(prob);
-
+    else {
+        label = results[0].className;
+        mobilenet.predict(gotResults);
     }
-}
-
-function imageReady() {
-    console.log("image is ready!");
-    image(dora, 0, 0);
 }
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(640, 550);
     background(0);
-    dora = createImg('images/dora.png', imageReady);
-    dora.hide();
-    mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+    video = createCapture(VIDEO);
+    video.hide();
+    mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
 }
 
 function draw() {
-
-
+    background(0);
+    image(video, 0, 0);
+    fill(255);
+    textSize(32);
+    text(label, 10, height - 20);
 }
