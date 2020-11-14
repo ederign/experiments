eaa8b0d745c5d838da915a036088738b99c07fed
Eder Ignatowicz <ignatowicz@gmail.com>
2020-11-14 11:14:08 -0500

    Part 2 KNN

--- CC_070.1_similarity_score/sketch.js
+++ CC_070.1_similarity_score/sketch.js
@@ -1,8 +1,9 @@
 let data;
-let dropdown1;
-let dropdown2;
+let dropdown1; 
 let resultP;
 const users = {};
+let similarityScores = {}
+let resultDivs = [];
 
 
 function preload() {
@@ -12,32 +13,65 @@ function preload() {
 function setup() {
     noCanvas();
 
-    dropdown1 = createSelect('');
-    dropdown2 = createSelect('');
+    dropdown1 = createSelect(''); 
     resultP = createP();
 
     for (let i = 0; i < data.users.length; i++) {
         const name = data.users[i].name;
         dropdown1.option(name);
-        dropdown2.option(name);
         users[name] = data.users[i];
     }
     var button = createButton('submit');
-    button.mousePressed(euclideanSimilarity);
+    button.mousePressed(findNearestNeighbors);
+}
+function findNearestNeighbors(){
+
+    for (let index = 0; index < resultDivs.length; index++) {
+        const div = resultDivs[index];
+        div.remove();    
+    }
+    resultDivs = [];
+
+    const name = dropdown1.value();
+
+    similarityScores = {};
+    for (let index = 0; index < data.users.length; index++) {
+        const other = data.users[index].name;
+        if(other != name){
+            let similarity = euclideanDistance(name, other)
+            similarityScores[other] = similarity;
+            data.users[index].similarityScore = similarity;
+        } else{
+            similarityScores[other] = -1;
+            data.users[index].similarityScore = -1;
+        }
+    }
+    data.users.sort(compareSimilarity);
+    
+    for (let i = 0; i < 5; i++) {
+        let div = createDiv(data.users[i].name + ': ' + data.users[i].similarityScore);
+        resultP.parent(div);
+        resultDivs.push(div);
+    }
 }
 
-function euclideanSimilarity() {
-    const ratings1 = users[dropdown1.value()];
-    const ratings2 = users[dropdown2.value()];
+function compareSimilarity(a, b){
+    let score1 = similarityScores[a.name];
+    let score2 = similarityScores[b.name];
+    return score2 - score1;
+}
+
+function euclideanDistance(name1, name2) {
+    const ratings1 = users[name1];
+    const ratings2 = users[name2];
 
     const titles = Object.keys(ratings1);
 
     titles.splice(titles.indexOf('name'), 1);
     titles.splice(titles.indexOf('timestamp'), 1);
-    console.log(titles);
 
     let sumSquares = 0;
-
+ 
     for (let index = 0; index < titles.length; index++) {
         const title = titles[index];
         const rating1 = ratings1[title];
@@ -54,5 +88,5 @@ function euclideanSimilarity() {
 
     const similarity = 1 / (1 + d);
 
-    resultP.html(similarity);
+    return similarity;
 }
