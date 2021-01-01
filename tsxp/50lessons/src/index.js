2f71652589ab97251b5ca42d5f50dd55ecf2e4ef
Eder Ignatowicz <ignatowicz@gmail.com>
2021-01-01 18:07:12 -0500

    50 lessons on ts - d1

---
+++ tsxp/50lessons/src/index.js
@@ -0,0 +1,27 @@
+//@ts-check
+const storage = {
+  max: undefined,
+  items: [],
+};
+
+Object.defineProperty(storage, "max", { writable: false, value: 5000 });
+
+let currentStorage = undefined;
+
+function storageUsed() {
+  if (currentStorage) {
+    return currentStorage;
+  }
+  currentStorage = 0;
+  for (let i = 0; i < storage.length(); i++) {
+    currentStorage += storage.items[i].weigth;
+  }
+  return currentStorage;
+}
+
+function add(item) {
+  if (storage.max - item.weight >= storageUsed()) {
+    storage.items.push(item);
+    currentStorage += item.weight;
+  }
+}
